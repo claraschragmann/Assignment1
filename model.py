40cf6f45ac8ba9f930c8c0077e022a7a9c702550
claraschragmann <144793871+claraschragmann@users.noreply.github.com>
2023-09-28 14:59:35 +0100

    Update model.py

--- model.py
+++ model.py
@@ -6,55 +6,31 @@ from pyfume.EstimateAntecendentSet import AntecedentEstimator
 from pyfume.EstimateConsequentParameters import ConsequentEstimator
 from pyfume.SimpfulModelBuilder import SugenoFISBuilder
 from pyfume.Tester import SugenoFISTester
-from numpy import copy
-from sklearn.metrics import accuracy_score, cohen_kappa_score
-from numpy import clip, column_stack, argmax
+from sklearn.metrics import accuracy_score, cohen_kappa_score, precision_score, recall_score
+from numpy import clip, column_stack, argmax, vectorize
+from sklearn import preprocessing
 from numpy import savetxt
-
+from sklearn.metrics import confusion_matrix
+import numpy as np
 # %% Load dataset and create train-test sets
 data = load_wine()
-
 X = data.data
 y = data.target
-print("Input:")
-print(X)
+
 min_max_scaler = preprocessing.MinMaxScaler()
 X = min_max_scaler.fit_transform(X)
-print("Input_normalized:")
-print(X)
+
 var_names = data.feature_names
-var_names = [var_names[i][0:-5] for i in range(0, len(var_names))]
 var_names = [var_names[i].title().replace(' ','') for i in range(0, len(var_names))]
 var_names = [var_names[i].title().replace('/','_') for i in range(0, len(var_names))]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=92)
 
-# is 0 where it is not Class 0
-y_0_vs_all = copy(y)
-y_0_vs_all[y_0_vs_all==0] = -1
-y_0_vs_all[y_0_vs_all!=-1] = 0
-y_0_vs_all[y_0_vs_all==-1] = 1
+# %% Train 0 vs all model
 
-# is 0 where it is not Class 1
-y_1_vs_all = copy(y)
-y_1_vs_all[y_1_vs_all==1] = -1
-y_1_vs_all[y_1_vs_all!=-1] = 0
-y_1_vs_all[y_1_vs_all==-1] = 1
-
-# is 0 where it is not Class 2
-y_2_vs_all = copy(y)
-y_2_vs_all[y_2_vs_all==2] = -1
-y_2_vs_all[y_2_vs_all!=-1] = 0
-y_2_vs_all[y_2_vs_all==-1] = 1
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-_, _, y_train_0_vs_all, _ = train_test_split(X, y_0_vs_all, test_size=0.2, random_state=42)
-_, _, y_train_1_vs_all, _ = train_test_split(X, y_1_vs_all, test_size=0.2, random_state=42)
-_, _, y_train_2_vs_all, _ = train_test_split(X, y_2_vs_all, test_size=0.2, random_state=42)
-
-
-# %% Train 0 vs all
-
+# Create 0-vs-all target vector
+y_train_0_vs_all = vectorize({0:1,1:0,2:0}.get)(y_train)
 # Cluster the input-output space
-cl = Clusterer(x_train=X_train, y_train=y_train_0_vs_all, nr_clus=10) #clusters have to be estimated
+cl = Clusterer(x_train=X_train, y_train=y_train_0_vs_all, nr_clus=10)
 clust_centers, part_matrix, _ = cl.cluster(method='fcm')
 # Estimate membership functions parameters
 ae = AntecedentEstimator(X_train, part_matrix)
@@ -62,13 +38,14 @@ antecedent_params = ae.determineMF()
 # Estimate consequent parameters
 ce = ConsequentEstimator(X_train, y_train_0_vs_all, part_matrix)
 conseq_params = ce.suglms()
-#geht bis hier
 # Build first-order Takagi-Sugeno model
 modbuilder = SugenoFISBuilder(antecedent_params, conseq_params, var_names, save_simpful_code=False)
 model_0_vs_all = modbuilder.get_model()
 
 # %% Train 1 vs all model
 
+# Create 1-vs-all target vector
+y_train_1_vs_all = vectorize({0:0,1:1,2:0}.get)(y_train)
 # Cluster the input-output space
 cl = Clusterer(x_train=X_train, y_train=y_train_1_vs_all, nr_clus=10)
 clust_centers, part_matrix, _ = cl.cluster(method='fcm')
@@ -85,6 +62,8 @@ model_1_vs_all = modbuilder.get_model()
 
 # %% Train 2 vs all model
 
+# Create 2-vs-all target vector
+y_train_2_vs_all = vectorize({0:0,1:0,2:1}.get)(y_train)
 # Cluster the input-output space
 cl = Clusterer(x_train=X_train, y_train=y_train_2_vs_all, nr_clus=10)
 clust_centers, part_matrix, _ = cl.cluster(method='fcm')
@@ -124,6 +103,9 @@ y_pred_probs = y_pred_probs/y_pred_probs.sum(axis=1,keepdims=1)
 
 y_pred = argmax(y_pred_probs,axis=1)
 
+print(y_test)
+print(y_pred)
+
 savetxt("predicted_classes.csv", y_pred, delimiter=",")
 
 # %% Compute classification metrics
@@ -131,3 +113,10 @@ acc_score = accuracy_score(y_test, y_pred)
 print("Accuracy: {:.3f}".format(acc_score))
 kappa = cohen_kappa_score(y_test, y_pred)
 print("Kappa Score: {:.3f}".format(kappa))
+
+cm = confusion_matrix(y_test, y_pred)
+recall = np.diag(cm) / np.sum(cm, axis = 1)
+precision = np.diag(cm) / np.sum(cm, axis = 0)
+
+print("Recall: {:.3f}".format(np.mean(recall)))
+print("Precision: {:.3f}".format(np.mean(precision)))
